5c10f72bd94726f4d277bbd237ae36d8376c11a3
Dario Hesami <Dario.Academic@gmail.com>
2024-05-24 09:47:34 -0400

    Week 3 - P1 - Mock Category model

--- DotNetBookStore/Controllers/CategoriesController.cs
+++ DotNetBookStore/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using DotNetBookStore.Models;
 
 namespace DotNetBookStore.Controllers
 {
@@ -6,11 +7,20 @@ namespace DotNetBookStore.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // use the Category model to generate 10 categories in memory for display in the view
+            var categories = new List<Category>();
+            for (int i = 1; i<11; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // must now pass the strongly-types data to the view
+            return View(categories);
         }
 
-        public IActionResult Browse()
+        public IActionResult Browse(string category)
         {
+            ViewBag.Category = category;
             return View();
         }
     }
---
+++ DotNetBookStore/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetBookStore.Models
+{
+    public class Category
+    {
+        // pk fields should always be called either {Model}Id or just Id
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
