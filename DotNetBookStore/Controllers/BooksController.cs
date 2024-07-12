d809aa9c6306897bd111a2a1bd1c01249c03b2c9
Dario Hesami <Dario.Academic@gmail.com>
2024-07-12 09:19:17 -0400

    Week 9 - p1 - Auth for Book - add Shop Controller

--- DotNetBookStore/Controllers/BooksController.cs
+++ DotNetBookStore/Controllers/BooksController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DotNetBookStore.Data;
 using DotNetBookstore.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DotNetBookStore.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class BooksController : Controller
     {
         private readonly ApplicationDbContext _context;
---
+++ DotNetBookStore/Controllers/ShopController.cs
@@ -0,0 +1,22 @@
+﻿using DotNetBookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetBookStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        // class level DbContext connection object
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // query the list of categories in a-z order for display
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}
